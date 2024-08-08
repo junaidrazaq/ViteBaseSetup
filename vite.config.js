6854f59c7fe593e57f45f79fd7bf69c6765f44da
junaidrazaq <junaid.razaq@hotmail.com>
2024-08-08 02:10:05 +0100

    eslint configurations

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
+import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), eslint()],
 });
