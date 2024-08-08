91681e9244126ce6ecb66d5debf6d19e9ce05b8e
junaidrazaq <junaid.razaq@hotmail.com>
2024-08-08 02:41:37 +0100

    mor eslint configurations

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import importPlugin from "eslint-plugin-import";
+import parser from "@typescript-eslint/parser";
 
 export default tseslint.config({
   extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -14,9 +16,22 @@ export default tseslint.config({
     globals: globals.browser,
   },
   plugins: {
+    import: importPlugin,
+    parser: parser,
     "react-hooks": reactHooks,
     "react-refresh": reactRefresh,
     "simple-import-sort": simpleImportSort,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./tsconfig.json",
+      },
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
   },
   rules: {
     ...reactHooks.configs.recommended.rules,
@@ -43,5 +58,15 @@ export default tseslint.config({
       "warn",
       { allowConstantExport: true },
     ],
+    "import/no-unresolved": "error",
+    "import/named": "error",
+    "import/default": "error",
+    "import/namespace": "error",
+    "import/no-restricted-paths": "error",
+    "import/no-absolute-path": "error",
+    "import/no-dynamic-require": "error",
+    "import/no-self-import": "error",
+    "import/no-cycle": "error",
+    "import/no-useless-path-segments": "error",
   },
 });
